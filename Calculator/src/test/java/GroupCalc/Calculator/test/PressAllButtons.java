dbb624982e99f7c375b36b1f0d001bf4ca7d57da
Denis Lit <teamovie@mail.ru>
2017-10-09 16:49:01 +0300

    Add test: press 1-9 buttons

--- Calculator/src/test/java/GroupCalc/Calculator/test/PressAllButtons.java
+++ Calculator/src/test/java/GroupCalc/Calculator/test/PressAllButtons.java
@@ -8,11 +8,13 @@ public class PressAllButtons extends TestBase{
 	@Test (priority=1)
 	 public void smokePlus() {
 	       
-	    for (int i=1; i<10; )
-	        app.press().button(i);
+	    for (int i=1; i<10; i++) {
+	        app.press().button(Integer.toString(i));
 	        app.press().clear();
-
 	    }
+	    }
+	
+	
 	
 	
 }
