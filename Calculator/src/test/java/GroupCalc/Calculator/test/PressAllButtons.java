0a0aa96909f9d6d4835eecc0079d15e06a5f5778
Denis Lit <teamovie@mail.ru>
2017-10-09 16:49:28 +0300

    +

--- Calculator/src/test/java/GroupCalc/Calculator/test/PressAllButtons.java
+++ Calculator/src/test/java/GroupCalc/Calculator/test/PressAllButtons.java
@@ -2,19 +2,15 @@ package GroupCalc.Calculator.test;
 
 import org.testng.annotations.Test;
 
-public class PressAllButtons extends TestBase{
+public class PressAllButtons extends TestBase {
 
-	
-	@Test (priority=1)
-	 public void smokePlus() {
-	       
-	    for (int i=1; i<10; i++) {
-	        app.press().button(Integer.toString(i));
-	        app.press().clear();
-	    }
-	    }
-	
-	
-	
-	
+	@Test(priority = 1)
+	public void pressFromOneToNine() {
+
+		for (int i = 1; i < 10; i++) {
+			app.press().button(Integer.toString(i));
+			app.press().clear();
+		}
+	}
+
 }
