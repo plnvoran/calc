8c8e1cc83656ca8a81a2fd03aaf16a2caa7b104e
Denis Lit <teamovie@mail.ru>
2017-10-09 17:53:20 +0300

    Smoke has been done.

--- Calculator/src/test/java/GroupCalc/Calculator/test/Smoke.java
+++ Calculator/src/test/java/GroupCalc/Calculator/test/Smoke.java
@@ -3,7 +3,14 @@ package GroupCalc.Calculator.test;
 import org.testng.annotations.Test;
 
 public class Smoke extends TestBase {
-  @Test
-  public void smokeTest() {
-  }
+	@Test
+	public void smokeTest() {
+
+		app.press().universalDigit("1");
+		app.press().sign("+");
+		app.press().universalDigit("2");
+		app.press().equality();
+		app.press().resultCheck("3");
+		app.press().clear();
+	}
 }
