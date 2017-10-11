4d2738cc066654ed338d08036a5b80a48afeb95e
Denis Lit <teamovie@mail.ru>
2017-10-11 13:57:51 +0300

    Add Minus2Operands test

---
+++ Calculator/src/test/java/GroupCalc/Calculator/test/Minus2Operands.java
@@ -0,0 +1,42 @@
+package GroupCalc.Calculator.test;
+
+import org.testng.annotations.Test;
+
+import GroupCalc.Calculator.ExelUtility.Constant;
+import GroupCalc.Calculator.ExelUtility.ExcelUtils;
+
+public class Minus2Operands extends TestBase {
+
+
+	 @Test
+	public void testMinus2Operands() throws Exception {
+		 
+		 String pathToFile = Constant.Path_TestData + Constant.Test2Operands;
+
+		ExcelUtils.setExcelFile(pathToFile, "Minus");
+
+		int i = 1;
+		while (!ExcelUtils.getCellData(i, 0).equals("")) {
+
+			app.press().universalDigit(ExcelUtils.getCellData(i, 0));
+			app.press().sign("-");
+			app.press().universalDigit(ExcelUtils.getCellData(i, 1));
+			app.press().equality();
+
+			if (app.press().resultCheckBoolean(ExcelUtils.getCellData(i, 2)) == true) {
+
+				ExcelUtils.setCellDataUn("Pass", i, 3, pathToFile);
+			}
+
+			else {
+
+				ExcelUtils.setCellDataUn("Fail", i, 3, pathToFile);
+			}
+
+			app.press().clear();
+
+			i++;
+		}
+
+	}
+}
